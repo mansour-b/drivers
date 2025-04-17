c2d8a0e2135d84ca45d2c13a468920b1db1cba2d
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-04-17 13:05:32 +0200

    class measuring circuit

---
+++ electronic_circuits/circuits.py
@@ -0,0 +1,11 @@
+from electronic_circuits.components import Component
+from electronic_circuits.drivers import Ammeter, DCVoltageGenerator, Voltmeter
+
+
+class MeasuringCircuit:
+    def __init__(self, component: Component):
+        self.component = component
+        self.generator = DCVoltageGenerator(component)
+
+        self.ammeter = Ammeter(component)
+        self.voltmeter = Voltmeter(component)
