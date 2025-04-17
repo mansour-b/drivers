ed71f8f2b3e47793139101f70ecd8fab5857b3e7
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-04-17 12:59:41 +0200

    update voltage generator so that they set voltage on components

--- electronic_circuits/drivers/generator.py
+++ electronic_circuits/drivers/generator.py
@@ -1,3 +1,9 @@
+from electronic_circuits.components import Component
+
+
 class DCVoltageGenerator:
+    def __init__(self, component: Component):
+        self.component = component
+
     def set_voltage(self, value: float) -> None:
-        pass
+        self.component.set_voltage(value)
