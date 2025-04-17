3b682fcff24ef47a896c574a3d4eee095f359387
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-04-17 10:09:39 +0200

    more realistic voltmeter and ammeter behaviours

--- drivers/ammeter.py
+++ drivers/ammeter.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+
 class Ammeter:
+    def __init__(self):
+        self.currents = np.arange(-5, 6) / 1e3
+        self.index = 0
+
     def measure(self) -> float:
-        return 1.0
+        res = self.currents[self.index]
+        self.index += 1
+        return res
--- drivers/voltmeter.py
+++ drivers/voltmeter.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+
 class Voltmeter:
+    def __init__(self):
+        self.voltages = np.arange(-5, 6)
+        self.index = 0
+
     def measure(self) -> float:
-        return 1.0
+        res = self.voltages[self.index]
+        self.index += 1
+        return res
