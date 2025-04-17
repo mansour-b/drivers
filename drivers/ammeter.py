61058497f4a4b121da02671f23d9b188265e0594
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-04-17 10:26:04 +0200

    add noise in current measurements

--- drivers/ammeter.py
+++ drivers/ammeter.py
@@ -6,6 +6,17 @@ class Ammeter:
         self.currents = np.arange(-5, 6) / 1e3
         self.index = 0
 
+        min_current = self.currents.min()
+        max_current = self.currents.max()
+
+        noise = (
+            0.1
+            * (max_current - min_current)
+            * np.random.rand(len(self.currents))
+            + min_current
+        )
+        self.currents += noise
+
     def measure(self) -> float:
         res = self.currents[self.index]
         self.index += 1
